b6613c2c20e543f8a5f5249e1cc06255db0b7594
Teo1273 <msa127306@gmail.com>
2025-02-27 07:18:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title("Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.writer("Facilmente puedo realizar backend y frontend.")
-image = Image.open ("porsche-911-gt3-rs-purple-beast-vorsteiner-18.jpg")
+image = Image.open ('porsche-911-gt3-rs-purple-beast-vorsteiner-18.jpg')
