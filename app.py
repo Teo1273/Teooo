586041160931493bb8ea3ded4382ac6663a2b484
Teo1273 <msa127306@gmail.com>
2025-02-27 07:11:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Mi Primera App!!")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.writer("Facilmente puedo realizar backend y frontend.")
