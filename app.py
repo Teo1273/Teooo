aac898ff53efd21ff9257f86347ff0c5228f267c
Teo1273 <msa127306@gmail.com>
2025-02-27 07:29:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.write('El texto escrito es', texto)
 
 st.subheader("Ahora usemos 2 columnas")
 
-col1, col2 = st.columna(2)
+col1, col2 = st.columns(2)
 
 with coll:
   st.subheader("Esta es la primera columna")
