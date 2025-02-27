3b6d5240cf04312cf0f3c90f83ff9d3f55145c32
Teo1273 <msa127306@gmail.com>
2025-02-27 07:20:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Mi Primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
-st.writer("Facilmente puedo realizar backend y frontend.")
+st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open ('porsche-911-gt3-rs-purple-beast-vorsteiner-18.jpg')
 
 st.image(image, caption='Interfaces multimodales')
